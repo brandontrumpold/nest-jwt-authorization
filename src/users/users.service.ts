145ab1da229ae4e1de21f97f7e21052b2f714a5d
brandontrumpold <brandon.trumpold@gmail.com>
2021-09-05 19:10:24 -0400

    init commit with local auth via passport

---
+++ src/users/users.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+
+export type User = {
+  id: number;
+  name: string;
+  username: string;
+  password: string;
+};
+
+@Injectable()
+export class UsersService {
+  private readonly users: User[] = [
+    {
+      id: 1,
+      name: 'Brandon',
+      username: 'brandon',
+      password: 'securepw',
+    },
+    {
+      id: 2,
+      name: 'Steve',
+      username: 'steve',
+      password: 'password',
+    },
+  ];
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find((user) => user.username === username);
+  }
+}
